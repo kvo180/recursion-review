92dcbb7c39133f1502a8167a6b7c9b500969f3a8
Hack Reactor Student <student@hackreactor.com>
2017-03-27 12:13:18 -0700

    Implemented stringifyJSON for basic arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,77 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var stringified = '';
+  //if obj is array
+  if (Array.isArray(obj)) {
+    var arr = [];
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      obj.forEach(function(item) {
+        return arr.push(stringifyJSON(item));
+      });
+    }
+    stringified += '[' + arr + ']';
+  
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"'; 
+  } else if (obj === null) {
+    return 'null';
+  
+  } else {
+    return String(obj);
+  }
+
+  
+  //if the array is empty
+   //return empty brackets
+  //iterate through array 
+   //stringify on each element
+ //if obj is object
+  //if obj is empty 
+   //retur brackets
+  //else iterate through object stringifying values
+   // if undefined || is fcuntion
+    // return {}
+  return stringified;
+ 
 };
+
+var t = [1, 2, true, 'helo'];
+
+console.log(stringifyJSON(t));
+
+/*
+9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+];
+
+
+unstringifiableValues = [
+  {
+    'functions': function() {},
+    'undefined': undefined
+  }
+];
+*/
+
