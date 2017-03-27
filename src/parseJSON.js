ac41fe7ff7281e83c6355bf7bce345b2b17bb8a5
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:41:21 -0700

    Added short skeleton for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,37 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+/*
 var parseJSON = function(json) {
-  // your code goes here
+
+var parseObj = function(obj) {
+    var parsedObj = {};
+
+    for (var i = 0; i < obj.length; i++) {
+
+    }
+}
+
+var parseArr = function(arr) {
+    var parsedArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        
+    }
+}
+
+var parseValue = function(val) {
+
+}
+
+
+var isObject = json.charAt(0) === '{' && json.charAt(json.length-1) === '}';
+
+if (isObject) {
+parseObj(json);
+} else {
+parseArr(json);
+}
+
 };
+*/
