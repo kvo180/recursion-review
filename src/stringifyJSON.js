436731d117e24cfb04d76aaf7d0168b0cf5b8b45
Hack Reactor Student <student@hackreactor.com>
2017-03-27 15:38:26 -0700

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,76 +5,51 @@
 
 var stringifyJSON = function(obj) {
   var stringified = '';
-  //if obj is array
-  if (Array.isArray(obj)) {
+
+  if (obj === null) {
+    return 'null';
+  
+
+  } else if (Array.isArray(obj)) {
     var arr = [];
     if (obj.length === 0) {
       return '[]';
+    
     } else {
       obj.forEach(function(item) {
-        return arr.push(stringifyJSON(item));
+        arr.push(stringifyJSON(item));
       });
     }
     stringified += '[' + arr + ']';
   
+
+  } else if (typeof obj === 'object') {
+    var objHolder = [];
+    
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    
+    } else {
+      for (var key in obj) {
+        if (obj[key] === undefined || typeof obj[key] === 'function') {
+          return '{}';
+        } else {
+          objHolder.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+        }
+      }
+      stringified += '{' + objHolder.join(',') + '}';
+    }
+  
+
   } else if (typeof obj === 'string') {
     return '"' + obj + '"'; 
-  } else if (obj === null) {
-    return 'null';
   
+
   } else {
     return String(obj);
   }
 
-  
-  //if the array is empty
-   //return empty brackets
-  //iterate through array 
-   //stringify on each element
- //if obj is object
-  //if obj is empty 
-   //retur brackets
-  //else iterate through object stringifying values
-   // if undefined || is fcuntion
-    // return {}
   return stringified;
  
 };
-
-var t = [1, 2, true, 'helo'];
-
-console.log(stringifyJSON(t));
-
-/*
-9,
-  null,
-  true,
-  false,
-  'Hello world',
-  [],
-  [8],
-  ['hi'],
-  [8, 'hi'],
-  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-  [8, [[], 3, 4]],
-  [[[['foo']]]],
-  {},
-  {'a': 'apple'},
-  {'foo': true, 'bar': false, 'baz': null},
-  {'boolean, true': true, 'boolean, false': false, 'null': null },
-  // basic nesting
-  {'a': {'b': 'c'}},
-  {'a': ['b', 'c']},
-  [{'a': 'b'}, {'c': 'd'}],
-  {'a': [], 'c': {}, 'b': true}
-];
-
-
-unstringifiableValues = [
-  {
-    'functions': function() {},
-    'undefined': undefined
-  }
-];
-*/
 
